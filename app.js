8ac0c8ee543d0245188f85bf45214f84011cd74b
Davis Clark <jdc0589@gmail.com>
2013-06-05 23:52:15 -0500

    added project root lookup so commands work from anywhere inside a project directory.

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var fs = require('fs')
   , repo = require('./makeRepo').RepoMaker
   , api = require('./makeApi').ApiMaker
   , app = require('./makeApp').AppMaker
-  , program = require ('commander');
+  , program = require ('commander')
+  , npm = require("npm")
+  , path = require("path");
 
 program
   .version('0.0.1')
@@ -18,15 +20,15 @@ program
   .description('Scaffold an API or Repository (or both)')
   .action(function(){
     if (program.repo) {
-      if (program.lite) {
-        repo.make(program.repo, true, process.cwd());
-      } else {
-        repo.make(program.repo, false, process.cwd());
-      }
+        appRoot(function(root) {
+          repo.make(program.repo, program.lite, root);
+        });
     }
 
     if (program.api) {
-      api.make(program.api, program.lite, process.cwd());
+      appRoot(function(root) {
+        api.make(program.api, program.lite, root);
+      });
     }
   });
 
@@ -34,7 +36,20 @@ program
   .command('new')
   .description('Scaffold a new Bogart app')
   .action(function(){
-    app.make(process.argv[3], false, process.cwd());
+    appRoot(function(root) {
+      app.make(process.argv[3], false, root);
+    });
   });
 
 program.parse(process.argv);
+
+
+function appRoot(cb) {
+  npm.load({}, function(er) {
+    if (er) {
+      throw er;
+    }
+
+    cb(path.join(npm.root, ".."));
+  });
+}
